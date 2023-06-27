fa8789fa504de073868c7130fa380264443637d6
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2023-06-27 03:27:21 -0400

    working on lab 3 q1

--- LAB_2/L02_022609127_Ogbomo.sql
+++ LAB_2/L02_022609127_Ogbomo.sql
@@ -3,7 +3,6 @@
  Date 06/01/2023
  Section DBS501NSC
  Instructor Riyadh Al-Essawi*/
-SET SERVEROUTPUT ON;
 
 --1. Write a store procedure called factorial that gets an integer number n and 
 --calculates and displays its factorial. You must use RECURSION to calculate the 
@@ -17,40 +16,41 @@ SET SERVEROUTPUT ON;
 --n! = fact(n) = n * (n-1) * (n-2) * . . . * 2 * 1
 --n! = fact(n) = n * (n-1)!
 
-CREATE OR REPLACE PROCEDURE factorial (x_num IN NUMBER,  x_fact out Number)
+CREATE OR REPLACE PROCEDURE factorial(x_num IN NUMBER, x_fact out Number)
 AS
 BEGIN
--- NUMBER IS 0 
+    -- NUMBER IS 0
 
-    IF(x_num = 0 )THEN
-    x_fact :=1;
+    IF (x_num = 0) THEN
+        x_fact := 1;
 
 -- NUM IS > 0
     ELSE
-    -- RECURSIVE CALL TO FIND FACTORIAL OF X_NUM
-    factorial(x_num-1,x_fact);
-    x_fact := x_num*x_fact;
+        -- RECURSIVE CALL TO FIND FACTORIAL OF X_NUM
+        factorial(x_num - 1, x_fact);
+        x_fact := x_num * x_fact;
     END IF;
 
-    EXCEPTION WHEN OTHERS 
-THEN
-DBMS_OUTPUT.put_line('Error. Something went wrong...');
+EXCEPTION
+    WHEN OTHERS
+        THEN
+            DBMS_OUTPUT.put_line('Error. Something went wrong...');
 END;
 
 DECLARE
-z number;
+    z number;
 
 BEGIN
-factorial(10,z);
-dbms_output.put_line(z);
-factorial(0,z);
-dbms_output.put_line(z);
-factorial(1,z);
-dbms_output.put_line(z);
-factorial(5,z);
-dbms_output.put_line(z);
-factorial(12,z);
-dbms_output.put_line(z);
+    factorial(10, z);
+    dbms_output.put_line(z);
+    factorial(0, z);
+    dbms_output.put_line(z);
+    factorial(1, z);
+    dbms_output.put_line(z);
+    factorial(5, z);
+    dbms_output.put_line(z);
+    factorial(12, z);
+    dbms_output.put_line(z);
 END;
 
 --Question 2
@@ -62,39 +62,40 @@ END;
 --f(2) = f(1) + f(0) = 1 + 0 = 1
 --f(3) = f(2) + f(1) = 1 + 1 = 2
 --f(4) = f(3) + f(2) = 2 + 1 = 3
---…
+--ï¿½
 --f(n) = f(n-1) + f(n-2)
 
-CREATE OR REPLACE PROCEDURE fibonacci (num_limit IN NUMBER, num_result out NUMBER)
+CREATE OR REPLACE PROCEDURE fibonacci(num_limit IN NUMBER, num_result out NUMBER)
 AS
 BEGIN
-    IF(num_limit < 0) THEN
+    IF (num_limit < 0) THEN
         DBMS_OUTPUT.put_line('Cant find the fibonacci number at a negative position...');
         RETURN;
     END IF;
-	num_result:=  ROUND(POWER(((1 +  SQRT(5)) / 2), num_limit) / sqrt(5));
-EXCEPTION WHEN OTHERS
-THEN
-DBMS_OUTPUT.put_line('Error. Something went wrong...');
+    num_result := ROUND(POWER(((1 + SQRT(5)) / 2), num_limit) / sqrt(5));
+EXCEPTION
+    WHEN OTHERS
+        THEN
+            DBMS_OUTPUT.put_line('Error. Something went wrong...');
 END;
 
 DECLARE
-x_sum NUMBER;
+    x_sum NUMBER;
 
 
 BEGIN
-fibonacci(0,x_sum);
-dbms_output.put_line(x_sum);
-fibonacci(2,x_sum);
-dbms_output.put_line(x_sum);
-fibonacci(3,x_sum);
-dbms_output.put_line(x_sum);
-fibonacci(4,x_sum);
-dbms_output.put_line(x_sum);
-fibonacci(10,x_sum);
-dbms_output.put_line(x_sum);
-fibonacci(38,x_sum);
-dbms_output.put_line(x_sum);
+    fibonacci(0, x_sum);
+    dbms_output.put_line(x_sum);
+    fibonacci(2, x_sum);
+    dbms_output.put_line(x_sum);
+    fibonacci(3, x_sum);
+    dbms_output.put_line(x_sum);
+    fibonacci(4, x_sum);
+    dbms_output.put_line(x_sum);
+    fibonacci(10, x_sum);
+    dbms_output.put_line(x_sum);
+    fibonacci(38, x_sum);
+    dbms_output.put_line(x_sum);
 END;
 
 
@@ -106,13 +107,13 @@ END;
 --to the current price if the price is greater than 0. The procedure shows the 
 --number of updated rows if the update is successful.
 --The procedure gets two parameters:
---• category_id IN NUMBER
---• amount NUMBER(9,2)
---To define the type of variables that store values of a table’ column, you can 
+--ï¿½ category_id IN NUMBER
+--ï¿½ amount NUMBER(9,2)
+--To define the type of variables that store values of a tableï¿½ column, you can 
 --also write:
 --vriable_name table_name.column_name%type;
 --The above statement defines a variable of the same type as the type of the 
---table’ column.
+--tableï¿½ column.
 --category_id products.category_id%type;
 --Or you need to see the table definition to find the type of the category_id 
 --column. Make sure the type of your variable is compatible with the value that 
@@ -125,43 +126,43 @@ END;
 --SQL%ROWCOUNT stores the number of rows affected by an INSERT, UPDATE, 
 --or DELETE.
 
-CREATE OR REPLACE PROCEDURE update_price_by_cat (target_category_id IN NUMBER, amount NUMBER)
+CREATE OR REPLACE PROCEDURE update_price_by_cat(target_category_id IN NUMBER, amount NUMBER)
 AS
-max_category PRODUCTS.CATEGORY_ID%type;
-rows_updated NUMBER;
+    max_category PRODUCTS.CATEGORY_ID%type;
+    rows_updated NUMBER;
 
-BEGIN 
--- GET THE MAX CATEGORY NUMBER AND ASSIGN IT TO MAX_CATEGORY
-SELECT MAX(CATEGORY_ID) 
-INTO max_category 
-FROM PRODUCTS;
+BEGIN
+    -- GET THE MAX CATEGORY NUMBER AND ASSIGN IT TO MAX_CATEGORY
+    SELECT MAX(CATEGORY_ID)
+    INTO max_category
+    FROM PRODUCTS;
 
 -- ASSERT
- IF(target_category_id <= 0 or target_category_id > max_category) THEN
-        dbms_output.put_line('The category:'|| target_category_id ||' does not exist...');
+    IF (target_category_id <= 0 or target_category_id > max_category) THEN
+        dbms_output.put_line('The category:' || target_category_id || ' does not exist...');
         RETURN;
- END IF;
-    
--- INCREASE LIST_PRICE OF TARGET CATEGORY BY AMOUNT 
-UPDATE 
-    PRODUCTS
-SET
-    LIST_PRICE = LIST_PRICE + amount
-WHERE CATEGORY_ID = target_category_id;
+    END IF;
 
-rows_updated := SQL%ROWCOUNT;
-DBMS_OUTPUT.put_line('number of rows affected: '|| rows_updated);
-EXCEPTION WHEN OTHERS
-THEN
-DBMS_OUTPUT.put_line('Error. Something went wrong...');
+-- INCREASE LIST_PRICE OF TARGET CATEGORY BY AMOUNT 
+    UPDATE
+        PRODUCTS
+    SET LIST_PRICE = LIST_PRICE + amount
+    WHERE CATEGORY_ID = target_category_id;
+
+    rows_updated := SQL%ROWCOUNT;
+    DBMS_OUTPUT.put_line('number of rows affected: ' || rows_updated);
+EXCEPTION
+    WHEN OTHERS
+        THEN
+            DBMS_OUTPUT.put_line('Error. Something went wrong...');
 END;
 
 BEGIN
-update_price_by_cat(5,10);
-update_price_by_cat(4,10);
-update_price_by_cat(3,10);
-update_price_by_cat(2,10);
-update_price_by_cat(1,10);
+    update_price_by_cat(5, 10);
+    update_price_by_cat(4, 10);
+    update_price_by_cat(3, 10);
+    update_price_by_cat(2, 10);
+    update_price_by_cat(1, 10);
 END;
 
 
@@ -178,11 +179,11 @@ END;
 --a variable of the same type. 
 --
 --If the average price is less than or equal to $1000, 
---update products’ price by 2% if the price of the product is less than the 
+--update productsï¿½ price by 2% if the price of the product is less than the 
 --calculated average. 
 --
 --If the average price is greater than $1000, update 
---products’ price by 1% if the price of the product is less than the calculated 
+--productsï¿½ price by 1% if the price of the product is less than the calculated 
 --average. 
 --
 --
@@ -190,51 +191,50 @@ END;
 --displays the number of updated rows.
 
 
-CREATE OR REPLACE PROCEDURE update_price_under_avg 
+CREATE OR REPLACE PROCEDURE update_price_under_avg
 AS
-average_price PRODUCTS.LIST_PRICE%type;
-rows_updated NUMBER;
+    average_price PRODUCTS.LIST_PRICE%type;
+    rows_updated  NUMBER;
 
-BEGIN 
--- GET THE AVERAGE PRICE FOR ALL ITEMS IN THE PRODUCTS TABLE COMBINED
-SELECT AVG(LIST_PRICE) 
-INTO average_price 
-FROM PRODUCTS;
+BEGIN
+    -- GET THE AVERAGE PRICE FOR ALL ITEMS IN THE PRODUCTS TABLE COMBINED
+    SELECT AVG(LIST_PRICE)
+    INTO average_price
+    FROM PRODUCTS;
 
-IF(average_price <= 1000) THEN 
-UPDATE 
-    PRODUCTS
-SET
-    LIST_PRICE = LIST_PRICE + (LIST_PRICE * (2/100))
-WHERE LIST_PRICE < average_price;
-END IF;
+    IF (average_price <= 1000) THEN
+        UPDATE
+            PRODUCTS
+        SET LIST_PRICE = LIST_PRICE + (LIST_PRICE * (2 / 100))
+        WHERE LIST_PRICE < average_price;
+    END IF;
 
-IF (average_price > 1000) THEN
-UPDATE 
-    PRODUCTS
-SET
-    LIST_PRICE = LIST_PRICE + (LIST_PRICE * (1/100))
-WHERE LIST_PRICE < average_price;
-END IF;
- 
-rows_updated := SQL%ROWCOUNT;
-DBMS_OUTPUT.put_line('number of rows affected: '|| rows_updated);
-EXCEPTION WHEN OTHERS
-THEN
-DBMS_OUTPUT.put_line('Error. Something went wrong...');
+    IF (average_price > 1000) THEN
+        UPDATE
+            PRODUCTS
+        SET LIST_PRICE = LIST_PRICE + (LIST_PRICE * (1 / 100))
+        WHERE LIST_PRICE < average_price;
+    END IF;
+
+    rows_updated := SQL%ROWCOUNT;
+    DBMS_OUTPUT.put_line('number of rows affected: ' || rows_updated);
+EXCEPTION
+    WHEN OTHERS
+        THEN
+            DBMS_OUTPUT.put_line('Error. Something went wrong...');
 END;
 
 BEGIN
-update_price_under_avg;
+    update_price_under_avg;
 END;
 
 --QUESTION 5
 --The company needs a report that shows three category of products basedtheir prices. 
 --The company needs to know if the product price is cheap, fair, orexpensive. 
---Let’s assume that? 
---If the list price is less thano (avg_price - min_price) / 2The product’s price is cheap.? 
---If the list price is greater thano (max_price - avg_price) / 2The product’ price is expensive.? 
---If the list price is betweeno (avg_price - min_price) / 2o ando (max_price - avg_price) / 2o the end values includedThe product’s price is fair.
+--Letï¿½s assume that? 
+--If the list price is less thano (avg_price - min_price) / 2The productï¿½s price is cheap.? 
+--If the list price is greater thano (max_price - avg_price) / 2The productï¿½ price is expensive.? 
+--If the list price is betweeno (avg_price - min_price) / 2o ando (max_price - avg_price) / 2o the end values includedThe productï¿½s price is fair.
 --Write a procedure named product_price_report to show the number of products in each price category:
 --The following is a sample output of the procedure if no error occurs:Cheap: 10Fair: 50Expensive: 18The values in the above examples are just random values and may not matchthe real numbers in your result.
 --The procedure has no parameter. 
@@ -244,56 +244,57 @@ END;
 
 CREATE OR REPLACE PROCEDURE product_price_report
 AS
-min_price NUMBER;
-avg_price NUMBER;
-max_price NUMBER;
-
-cheap_item NUMBER := 0;
-fair_item NUMBER  :=0;
-expensive_item NUMBER :=0;
-
-CURSOR product_cursor 
-IS
-SELECT
-PRODUCT_ID ,
-PRODUCT_NAME ,
-DESCRIPTION ,
-STANDARD_COST ,
-LIST_PRICE ,
-CATEGORY_ID 
-FROM PRODUCTS;
+    min_price      NUMBER;
+    avg_price      NUMBER;
+    max_price      NUMBER;
+    cheap_item     NUMBER := 0;
+    fair_item      NUMBER := 0;
+    expensive_item NUMBER := 0;
+    CURSOR product_cursor
+        IS
+        SELECT PRODUCT_ID,
+               PRODUCT_NAME,
+               DESCRIPTION,
+               STANDARD_COST,
+               LIST_PRICE,
+               CATEGORY_ID
+        FROM PRODUCTS;
 
 BEGIN
-SELECT MIN(LIST_PRICE), 
-AVG(LIST_PRICE),
-MAX(LIST_PRICE)
+    SELECT MIN(LIST_PRICE),
+           AVG(LIST_PRICE),
+           MAX(LIST_PRICE)
 
-INTO 
-min_price, 
-avg_price,
-max_price 
+    INTO
+        min_price,
+        avg_price,
+        max_price
 
-FROM PRODUCTS;
+    FROM PRODUCTS;
 
-FOR item in product_cursor LOOP
-IF(item.list_price < (avg_price - min_price) /2) THEN 
-cheap_item := cheap_item +1;
+    FOR item in product_cursor
+        LOOP
+            IF (item.list_price < (avg_price - min_price) / 2) THEN
+                cheap_item := cheap_item + 1;
 
-ELSIF(item.list_price > (max_price - avg_price) /2) THEN
-expensive_item := expensive_item +1;
+            ELSIF (item.list_price > (max_price - avg_price) / 2) THEN
+                expensive_item := expensive_item + 1;
 
-ELSIF(item.list_price >= ((avg_price - min_price) /2) AND item.list_price <= ((max_price - avg_price)/2)) THEN
-fair_item := fair_item +1;
-END IF;
-END LOOP;
+            ELSIF (item.list_price >= ((avg_price - min_price) / 2) AND
+                   item.list_price <= ((max_price - avg_price) / 2)) THEN
+                fair_item := fair_item + 1;
+            END IF;
+        END LOOP;
 
-DBMS_OUTPUT.put_line('Cheap: '|| cheap_item || chr(10) ||'Fair: ' || fair_item || chr(10) || 'Expensive: '|| expensive_item);
+    DBMS_OUTPUT.put_line('Cheap: ' || cheap_item || chr(10) || 'Fair: ' || fair_item || chr(10) || 'Expensive: ' ||
+                         expensive_item);
 
-EXCEPTION WHEN OTHERS 
-THEN
-DBMS_OUTPUT.put_line('Error. Something went wrong...');
+EXCEPTION
+    WHEN OTHERS
+        THEN
+            DBMS_OUTPUT.put_line('Error. Something went wrong...');
 END;
 
 BEGIN
-product_price_report;
+    product_price_report;
 END;
--- LAB_3/lab_3.sql
+++ LAB_3/lab_3.sql
@@ -1,210 +1,77 @@
 CREATE TYPE days_array AS varray(31) OF INTEGER;
 CREATE TYPE months_array AS varray(12) OF INTEGER;
 CREATE OR REPLACE PROCEDURE array_date(days_list days_array, months_list months_array)
-AS
-DECLARE
-    max_days   NUMBER := 31;
-    max_months NUMBER := 12;
+    IS
+    days_limit   NUMBER := 31;
+    months_limit NUMBER := 12;
 
     too_many_days EXCEPTION;
     too_many_months EXCEPTION;
-    invalid_month EXCEPTION;
     invalid_day EXCEPTION;
-    empty_array EXCEPTION;
+    invalid_month EXCEPTION;
 
-    -- GET THE CURRENT YEAR
-    year         NUMBER  := (EXTRACT(YEAR FROM SYSDATE));
-
-    -- CREATE AN EXCEPTION FOR FRACTIONAL VALUES
-    -- MONTHS AND DAYS CANNOT BE FRACTIONAL
-
-    -- BOOLEAN TO DETERMINE IF THE YEAR IS A LEAP-YEAR
-    is_leap_year BOOLEAN := FALSE ;
-
-    -- MAX DAYS FOR EACH MONTH
-    max_days_jan NUMBER  := 31;
-    max_days_feb NUMBER  := 28;
-    max_days_mar NUMBER  := 31;
-    max_days_apr NUMBER  := 30;
-    max_days_may NUMBER  := 31;
-    max_days_jun NUMBER  := 30;
-    max_days_jul NUMBER  := 31;
-    max_days_aug NUMBER  := 31;
-    max_days_sep NUMBER  := 30;
-    max_days_oct NUMBER  := 31;
-    max_days_nov NUMBER  := 30;
-    max_days_dec NUMBER  := 31;
-
-    -- MONTH NUMBERS
-    jan          NUMBER  := 1;
-    feb          NUMBER  := 2;
-    mar          NUMBER  := 3;
-    apr          NUMBER  := 4;
-    may          NUMBER  := 5;
-    jun          NUMBER  := 6;
-    jul          NUMBER  := 7;
-    aug          NUMBER  := 8;
-    sep          NUMBER  := 9;
-    oct          NUMBER  := 10;
-    nov          NUMBER  := 11;
-    dec          NUMBER  := 12;
+    -- VARIABLES TO HOLD THE DATE
+    day_in_week VARCHAR2(15 CHAR);
+    year        NUMBER := TO_NUMBER(EXTRACT(YEAR FROM SYSDATE));
+    target_date DATE;
 BEGIN
     -- GET SIZE OF TWO ARRAYS
+    -- IF THE DAYS ARRAY HAS MORE THAN 31 VALUES
+    -- THROW AN EXCEPTION
 
-    IF (days_list.COUNT = 0 OR months_list.COUNT = 0) THEN
-        RAISE empty_array;
-    END IF;
-
-    -- IF THE DAYS ARRAY HAS MORE THAN 31 VALS
-    -- THROW AN EXCEPTION 
-
-    IF (days_list.COUNT >= max_days) THEN
+    IF (days_list.COUNT >= days_limit) THEN
         RAISE too_many_days;
     END IF;
 
-
     -- IF THE MONTHS ARRAY HAS MORE THAN 12 DAYS
-    -- THROW AN EXCEPTION 
+    -- THROW AN EXCEPTION
 
-    IF (months_list.COUNT >= max_months) THEN
+    IF (months_list.COUNT >= months_limit) THEN
         RAISE too_many_months;
     END IF;
 
-
-    IF (MOD(year, 4) = 0) THEN
-        IF (MOD(year, 100) = 0) THEN
-            IF (MOD(year, 400)) THEN
-                is_leap_year := TRUE;
-            ELSE
-                is_leap_year := FALSE;
-            END IF;
-        ELSE
-            is_leap_year := TRUE;
-        END IF;
-    END IF;
-
-
-    IF (is_leap_year = TRUE) THEN
-        max_days_feb := 29;
-    END IF;
-
-
 -- TRAVERSE THE ARRAY OF DAYS AND MONTHS AND EVALUATE EACH INDEX AGAINST THE INVARIANT TO GET THE DATE
 
-    FOR target_month in months_list
+    FOR i in months_list.FIRST..months_list.LAST
         LOOP
-            IF (target_month <= 0 || target_month > max_months) THEN
+            IF (months_list(i) <= 0 OR months_list(i) > 12) THEN
                 RAISE invalid_month;
             END IF;
-            FOR target_day in days_list
+
+            FOR j in days_list.FIRST..days_list.LAST
                 LOOP
-                    CASE
-                        WHEN target_month = jan THEN {
-                    -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            IF (target_day <= 0 OR target_day > max_days_jan) THEN
-                                RAISE invalid_day;
-                            END IF;
-
-                            DBMS_OUTPUT.PUT_LINE('January ' || target_day || ', ' || year || CHR(10));
-                            }
-                        WHEN target_month = feb THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('FEB ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = mar THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('MAR ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = apr THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('APR ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = may THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('MAY ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = jun THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('JUN ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = jul THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('JUL ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = aug THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('AUG ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = sep THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('SEP ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = oct THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('OCT ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = nov THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('NOV ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        WHEN target_month = dec THEN {
-                -- IF THE DAY IS GREATER THAN THE MAX DAYS FOR JAN 
-                        --THROW AN EXCEPTION 
-                            DBMS_OUTPUT.PUT_LINE('DEC ' || target_day || ', ' || year || CHR(10));
-
-                            }
-                        ELSE RAISE CASE_NOT_FOUND;
-                        END CASE;
+                    IF (days_list(j) > TO_NUMBER((EXTRACT(DAY FROM LAST_DAY((EXTRACT(YEAR FROM SYSDATE)) || '-' ||
+                                                                            (months_list(i)) || '-' ||
+                                                                            (days_list(j))))))) THEN
+                        RAISE invalid_day;
+                    END IF;
+                    target_date := TO_DATE(
+                                TO_CHAR(TO_DATE(months_list(months_list(i)), 'MM'), 'MON') || ' ' ||
+                                days_list(j) || ' ' ||
+                                year, 'MON DD YYYY');
+                    day_in_week := TO_CHAR(target_date, 'DAY');
+                    DBMS_OUTPUT.put_line(day_in_week || ', ' || TO_CHAR(target_date, 'Month') || ' ' || days_list(j) ||
+                                         ', ' || year ||
+                                         CHR(10));
                 END LOOP;
         END LOOP;
 
 EXCEPTION
     WHEN
-        CASE_NOT_FOUND THEN DBMS_OUTPUT.PUT_LINE('Invalid month...' || CHR(10));
-
-    WHEN
-        too_many_days THEN DBMS_OUTPUT.put_line('The maximum amount of days permitted is ' || max_days || CHR(10) ||
-                                                'You entered: ' || days_list.COUNT || CHR(10));
-
-    WHEN
-        too_many_months THEN DBMS_OUTPUT.put_line('The maximum amount of months permitted is ' || max_months ||
-                                                  CHR(10) || 'You entered: ' || months_list.COUNT || CHR(10));
-
-    WHEN
-        invalid_month THEN DBMS_OUTPUT.put_line('The month: ' || || ' is invalid...' || CHR(10)); --invalid month
-
-    WHEN
-        invalid_day THEN DBMS_OUTPUT.put_line('The day: ' || || ' is invalid...' || CHR(10)); --invalid day
-
-    WHEN
-        empty_array THEN DBMS_OUTPUT.put_line('An array is empty' || CHR(10));
-
+        invalid_day THEN
+        DBMS_OUTPUT.put_line('You have entered an invalid day' || CHR(10));
+    WHEN too_many_days THEN
+        DBMS_OUTPUT.put_line('You have entered too many day values.' || CHR(10) || 'The amount permitted is: ' ||
+                             days_limit || CHR(10));
+    WHEN too_many_months THEN
+        DBMS_OUTPUT.put_line('You have entered too many month values.' || CHR(10) || 'The amount permitted is: ' ||
+                             months_limit || CHR(10));
+    WHEN invalid_month THEN
+        DBMS_OUTPUT.put_line('You have entered an invalid month.' || CHR(10) || 'The months range from 1-12: ' ||
+                             CHR(10));
     WHEN
         OTHERS THEN DBMS_OUTPUT.put_line('Oops something went wrong...' || CHR(10));
 END;
-
-DECLARE
 
 BEGIN
     array_date((1, 5, 7), (2, 4));
